c4a595e1283bfcbd00d6568da08c731c2539a12f
mayumiyamada <y.mayumi1011@gmail.com>
2020-12-11 11:22:06 +0900

    room_usersテーブルの作成

--- db/migrate/20201210075515_create_rooms.rb
+++ db/migrate/20201210075515_create_rooms.rb
@@ -1,9 +1,9 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-
+      t.references :room, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
-      t.string :name, null:false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_075515) do
+ActiveRecord::Schema.define(version: 2020_12_10_080728) do
+
+  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
